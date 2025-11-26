d67e2a7e52443283d217d386f50db3211f2a956e
Unnati mishra <unnati10mishra@gmail.com>
2025-11-26 14:31:10 +0530

    Update fileio.c

--- fileio.c
+++ fileio.c
@@ -1,21 +1,21 @@
 #include "fileio.h"
 #include <stdio.h>
 #include <stdlib.h>
-void saveTransactions(Transaction *array, int size) {
+void saveTransactions(transaction *array, int size) {
     FILE *fp = fopen("transactions.dat", "wb");
     if (fp) {
-        fwrite(array, sizeof(Transaction), size, fp);
+        fwrite(array, sizeof(transaction), size, fp);
         fclose(fp);
     }
 }
-int loadTransactions(Transaction **array, int *capacity) {
+int loadTransactions(transaction **array, int *capacity) {
     FILE *fp = fopen("transactions.dat", "rb");
     if (!fp) return 0;
     fseek(fp, 0, SEEK_END);
-    int size = ftell(fp) / sizeof(Transaction);
+    int size = ftell(fp) / sizeof(transaction);
     rewind(fp);
-    *array = malloc(size * sizeof(Transaction));
-    fread(*array, sizeof(Transaction), size, fp);
+    *array = malloc(size * sizeof(transaction));
+    fread(*array, sizeof(transaction), size, fp);
     *capacity = size > 0 ? size * 2 : 10;
     fclose(fp);
     return size;
