ea40f39ecc373b889349ea38c63a295993d22174
AKSHAT PANDEY <25lbcs3024@culkomail.in>
2025-11-26 14:09:03 +0530

    Update_Akshat

---
+++ transaction.h
@@ -0,0 +1,9 @@
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+
+#include "toll.h"
+
+void searchTransaction(Transaction *array, int size);
+void summaryReport(Transaction *array, int size);
+
+#endif
