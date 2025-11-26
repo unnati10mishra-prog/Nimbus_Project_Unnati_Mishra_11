cdca0407be988609db386f464e301253b62da763
Unnati mishra <unnati10mishra@gmail.com>
2025-11-26 14:29:49 +0530

    Update transaction.h

--- transaction.h
+++ transaction.h
@@ -3,7 +3,7 @@
 
 #include "toll.h"
 
-void searchTransaction(Transaction *array, int size);
-void summaryReport(Transaction *array, int size);
+void searchTransaction(transaction *array, int size);
+void summaryReport(transaction *array, int size);
 
 #endif
